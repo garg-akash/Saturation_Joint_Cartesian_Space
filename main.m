3fbf2139c5fc2d965f0ec258c2e3865036ceaffc
garg-akash <akashgrg2@gmail.com>
2020-09-07 19:25:09 +0530

    compute cartesian pos vel acc for all joints

--- main.m
+++ main.m
@@ -43,6 +43,30 @@ for i = 1:nSteps
     X_d(i,:) = [x(i) y(i) z_0];
     dX_d(i,:) = [dxt(i) dyt(i) dzt_0];
 end
+%% Symbolic Jacobian
+JCarsym1 = []; JdotCarsym1=[]; 
+JCarsym2 = []; JdotCarsym2=[];
+JCarsym3 = []; JdotCarsym3=[];
+JCarsym4 = []; JdotCarsym4=[];
+JCarsym5 = []; JdotCarsym5=[];
+JCarsym6 = []; JdotCarsym6=[];
+alpha = [pi/2,-pi/2,-pi/2,pi/2,pi/2,-pi/2,0];
+a = [0,0,0,0,0,0,0];
+d = [sym('d1'),0,sym('d3'),0,sym('d5'),0,sym('d7')];
+qsym = [sym('q1'),sym('q2'),sym('q3'),sym('q4'),sym('q5'),sym('q6'),0];
+dqsym = [sym('dq1');sym('dq2');sym('dq3');sym('dq4');sym('dq5');sym('dq6')]; %these are the joint velocities at evaluation point
+[JCarsym1, JdotCarsym1] = computeJacobianSym(alpha(1:1),a(1:1),...
+    d(1:1),qsym(1:1),dqsym(1:1));
+[JCarsym2, JdotCarsym2] = computeJacobianSym(alpha(1:2),a(1:2),...
+    d(1:2),qsym(1:2),dqsym(1:2));
+[JCarsym3, JdotCarsym3] = computeJacobianSym(alpha(1:3),a(1:3),...
+    d(1:3),qsym(1:3),dqsym(1:3));
+[JCarsym4, JdotCarsym4] = computeJacobianSym(alpha(1:4),a(1:4),...
+    d(1:4),qsym(1:4),dqsym(1:4));
+[JCarsym5, JdotCarsym5] = computeJacobianSym(alpha(1:5),a(1:5),...
+    d(1:5),qsym(1:5),dqsym(1:5));
+[JCarsym6, JdotCarsym6] = computeJacobianSym(alpha(1:6),a(1:6),...
+    d(1:6),qsym(1:6),dqsym(1:6));
 %% Robot parameters for the KUKA-LWR
 m = 3;
 nJoints = 6;
@@ -108,6 +132,47 @@ for i = 1:nSteps
     %task2.Jsym = eye(n);
     task2.Jdot = zeros(nJoints);
     tasks = [task1;task2];
+    %%%
+    Parray(:,:,i) = robot.computeJointPositionArray;
+    q1 = q(1);
+    q2 = q(2);
+    q3 = q(3);
+    q4 = q(4);
+    q5 = q(5);
+    q6 = q(6);
+    
+    dq1 = dq(1);
+    dq2 = dq(2);
+    dq3 = dq(3);
+    dq4 = dq(4);
+    dq5 = dq(5);
+    dq6 = dq(6);
+    symvalues = [d1 d3 d5 d7 q1 q2 q3 q4 q5 q6 dq1 dq2 dq3 dq4 dq5 dq6];
+    JCar1 = double(subs(JCarsym1));
+    JdotCar1 = double(subs(JdotCarsym1));
+    JCar2 = double(subs(JCarsym2));
+    JdotCar2 = double(subs(JdotCarsym2));
+    JCar3 = double(subs(JCarsym3));
+    JdotCar3 = double(subs(JdotCarsym3));
+    JCar4 = double(subs(JCarsym4));
+    JdotCar4 = double(subs(JdotCarsym4));
+    JCar5 = double(subs(JCarsym5));
+    JdotCar5 = double(subs(JdotCarsym5));
+    JCar6 = double(subs(JCarsym6));
+    JdotCar6 = double(subs(JdotCarsym6));
+    dP1(:,i) = JCar1*dq(1:1,i);
+    ddP1(:,i) = JdotCar1*dq(1:1,i) + JCar1*ddq(1:1,i);
+    dP2(:,i) = JCar2*dq(1:2,i);
+    ddP2(:,i) = JdotCar2*dq(1:2,i) + JCar2*ddq(1:2,i);
+    dP3(:,i) = JCar3*dq(1:3,i);
+    ddP3(:,i) = JdotCar3*dq(1:3,i) + JCar3*ddq(1:3,i);
+    dP4(:,i) = JCar4*dq(1:4,i);
+    ddP4(:,i) = JdotCar4*dq(1:4,i) + JCar4*ddq(1:4,i);
+    dP5(:,i) = JCar5*dq(1:5,i);
+    ddP5(:,i) = JdotCar5*dq(1:5,i) + JCar5*ddq(1:5,i);
+    dP6(:,i) = JCar6*dq(1:6,i);
+    ddP6(:,i) = JdotCar6*dq(1:6,i) + JCar6*ddq(1:6,i);
+    %%%
     %T_stack_c(:,i) = getTorques(tasks, M, cc, g, dq(:,i), m, nJoints);%qsym, dqsym, symvalues
     %T_stack_c = J'*f_pos_star + n - S*dq(:,i);
     T_stack_c(:,i) = scs(M, cc, g, dq(:,i), J, Jdot, m, nJoints, Pee(1:3,i), dPee(:,i), dt, X_max, X_min, V_max, V_min, A_max, A_min, tasks, i);
@@ -118,6 +183,12 @@ for i = 1:nSteps
     dq(:,i+1) = dq(:,i) + ddq(:,i)*dt;
     q(:,i+1) = q(:,i) + dq(:,i)*dt + 0.5*ddq(:,i)*dt*dt;
 end
+P1 = squeeze(Parray(:,1,:));
+P2 = squeeze(Parray(:,2,:));
+P3 = squeeze(Parray(:,3,:));
+P4 = squeeze(Parray(:,4,:));
+P5 = squeeze(Parray(:,5,:));
+P6 = squeeze(Parray(:,6,:));
 size_Pee = size(Pee,2);
 % figure;
 % plot(tSteps(1:size_Pee),Pee(1,:));
